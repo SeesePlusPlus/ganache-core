dc5c956d5d075c894a09ddf057255aae14be1b9f
Mike Seese <seesemichaelj@gmail.com>
2017-12-14 02:31:45 -0500

    add more complex nature to the debug contract

--- test/DebugContract.sol
+++ test/DebugContract.sol
@@ -5,6 +5,14 @@ contract DebugContract {
   uint public value = 5;
 
   function setValue(uint _val) {
-    value = _val;
+    uint256 newVal = 108;
+    uint256 nextVal = newVal / 2;
+    newVal += 1;
+    uint256 priorVal = nextVal * _val;
+    value = calcValue(priorVal);
+  }
+
+  function calcValue(uint _val) returns (uint256) {
+    return _val * 2;
   }
 }
